7535bec333f9cd28c13a051582171445f3456818
aadya909 <145979679+aadya909@users.noreply.github.com>
2025-03-30 23:50:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
+function firstNonRepeatedChar(text) {
+  const charCount = {};
+  
+  for (const char of text) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+  
+  for (const char of text) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+  
+  return null;
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
